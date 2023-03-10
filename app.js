bd631e9bce534c4bfcc487408e4888d2eaaeac1c
Lina <104576477+poly-nomial@users.noreply.github.com>
2023-03-10 18:23:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://polybitfilms.nomoredomains.rocks');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
